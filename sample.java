a612a8cdf36564480066ad5c3a4a83a5725f104f
myleshwarsenthil <myleshwarsenthil@gmail.com>
2026-04-07 20:35:27 +0530

    Sample array program

---
+++ sample.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class sample{
+    public static void main(String[] args) {
+        Scanner  sc = new Scanner(System.in);
+        System.out.println("----Array Program!----");
+        System.out.print("Enter the array size: ");
+        int size =  sc.nextInt();
+        int [] arr=new int[size];
+        for(int i=0;i<size;i++){
+            System.out.print("Enter the array value "+(i+1)+":");
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Array elements are: ");
+        for(int j=0;j<arr.length;j++){
+            System.out.println(arr[j]);
+        }
+    }
+}
