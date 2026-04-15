6307a41ebfe4369d04d0514c60fad6fe89276454
myleshwarsenthil <myleshwarsenthil@gmail.com>
2026-04-15 21:16:22 +0530

    Small Changes

--- factorial.java
+++ factorial.java
@@ -1,15 +1,17 @@
 import java.util.*;
 
-class factorial{
+public class factorial{
     public static void main(String[] args){
+        System.out.println("---Welcome to Factorial operation---");
         oper o = new oper();
         String user;
         do { 
             o.dooper();
             Scanner scan=new Scanner(System.in);
-            System.out.print("Do you want to continue: ");
+            System.out.print("Do you want to continue: \nIf yes : \"y or Y\"\nIf no: \"n or N\" ");
             user = scan.nextLine();
-        } while (user.equals("y"));
+        } while (user.equals("y")||user.equals("Y"));
+        System.out.println("Thanks Bye...");
     }
 }
 
@@ -21,6 +23,6 @@ class oper{
         for(int i=a-1;i>0;i--){
             a = a*i;
         }
-        System.out.println(a);
+        System.out.println("Factorial is: "+a);
     }
 }
