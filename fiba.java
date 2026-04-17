a462a9fb71e9e9f58a6f6803774bcbdad51e90c3
myleshwarsenthil <myleshwarsenthil@gmail.com>
2026-04-17 21:13:44 +0530

    some changes

--- fiba.java
+++ fiba.java
@@ -1,17 +1,35 @@
+import java.util.*;
 
 class fiba{
     public static void main(String[] args) {
+        oper o=new oper();
+        String user;
+        do { 
+            Scanner sc=new Scanner(System.in);
+            o.dooper();
+            System.out.print("Do you want to continue: ");
+            user = sc.nextLine();
+        } while (user.equals("y"));
+    }
+}
+
+class oper{
+    public  void dooper(){
+        Scanner sc=new Scanner(System.in);
         int a=0;
         int b=1;
         int c;
-        
-        System.out.print(a);
-        System.out.print(b);
-        for (int i=0;i<=10;i++){
+        System.out.print("Enter the range: ");
+        int user = sc.nextInt();
+        System.out.print(a+ ", ");
+        System.out.print(b+", ");
+        for (int i=0;i<=user;i++){
             c=a+b;
-            System.out.print(c);
+            System.out.print(c+", ");
             a=b;
             b=c;
         } 
+        
+            System.out.println(" ");
     }
 }
