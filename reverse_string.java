83a3f4d9d73579a134561152cd3742f233efa912
myleshwarsenthil <myleshwarsenthil@gmail.com>
2026-04-18 20:44:41 +0530

    Some updates

--- reverse_string.java
+++ reverse_string.java
@@ -3,6 +3,20 @@ import java.util.*;
 class reverse_string{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        String user ="";
+        oper o=new oper();
+        do { 
+            o.dooper();
+            System.out.print("Do you want to continue: ");
+            user = sc.nextLine();
+        } while (user.equals("y"));
+     
+    }
+}
+
+class oper{
+    public void dooper(){
+        Scanner sc=new Scanner(System.in);
         System.out.print("Enter a string: ");
         String user = sc.nextLine();
         String output = "";
