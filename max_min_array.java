9396ca813daed47ce6e827bfe7992e46c3b22f72
myleshwarsenthil <myleshwarsenthil@gmail.com>
2026-04-18 20:39:02 +0530

    with user input and continuation

--- max_min_array.java
+++ max_min_array.java
@@ -1,7 +1,30 @@
+import java.util.*;
 
 class max_min_array{
     public static void main(String[] args){
-        int [] arr= {1,2,3,4,5,4,8,3};
+        dooper d=new dooper();
+        String user="";
+        do { 
+            d.oper();
+            Scanner sc=new Scanner(System.in);
+            System.out.print("Do you want to continue: ");
+            user = sc.nextLine();
+        } while (user.equals("y"));
+    }
+}
+
+class dooper{
+    public void oper(){
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int size = sc.nextInt();
+        int [] arr =new int[size];
+        for (int k=0; k<size; k++){
+            System.out.print("Enter array value "+ (k+1)+": ");
+            arr [k]=sc.nextInt();
+
+        }
         int temp=0;
         long tempmin=999999999;
         for(int i=0; i<=arr.length-1; i++){
@@ -17,5 +40,4 @@ class max_min_array{
         System.out.println("Smallest element in array is: "+tempmin);
         System.out.println("Largest element in array is: "+temp);
     }
-    
 }
