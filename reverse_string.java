8cf6a63dd610718cdd59c6ebfbfaa66b8f19fc34
myleshwarsenthil <myleshwarsenthil@gmail.com>
2026-04-18 20:06:48 +0530

    Reverse string program

---
+++ reverse_string.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class reverse_string{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String user = sc.nextLine();
+        String output = "";
+        for (int i=user.length()-1; i>=0; i--){
+            output = output +user.charAt(i);
+        }
+        System.out.println("Reversed string is: "+output);
+    }
+}
