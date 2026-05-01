b3b15b45dda0bc19b9ba6904673bf4f7d11619df
myleshwarsenthil <myleshwarsenthil@gmail.com>
2026-05-01 13:43:01 +0530

    practice codes for reference

---
+++ practice.java
@@ -0,0 +1,11 @@
+class practice{
+    public static void calculate(int a, int b){
+        int result = a+b;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args){
+        calculate(10, 20);
+        calculate(23, 32);
+    }
+}
