3deb41a40d629b4c6095df056b61b5043b0034f6
myleshwarsenthil <myleshwarsenthil@gmail.com>
2026-05-01 13:46:51 +0530

    another program to refer

--- practice.java
+++ practice.java
@@ -1,11 +1,15 @@
 class practice{
-    public static void calculate(int a, int b){
-        int result = a+b;
-        System.out.println(result);
+    static int sum(int x, int y){
+        return x+y;
+    }
+
+    static int sub(int x, int y){
+        return x-y;
     }
 
     public static void main(String[] args){
-        calculate(10, 20);
-        calculate(23, 32);
+        int su= sum(10, 20);
+        int s=sub(23, 13);
+        System.out.println(su+" "+s);
     }
 }
