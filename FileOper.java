d2e6c8bf89aff8f3e0450075c205b5e5043254c2
myleshwarsenthil <myleshwarsenthil@gmail.com>
2026-05-04 20:13:59 +0530

    File creation, Reading, Writing code

--- FileOper.java
+++ FileOper.java
@@ -1,15 +1,40 @@
-import  java.io.*;
+import java.io.*;
+import java.util.*;
 
-class FileOper{
+class kuppa{
     public static void main(String[] args){
+        File f = new File("summa1.txt");
         try{
-        FileWriter f = new FileWriter("waste.txt");
-        f.write("This contains nothing!");
-        f.close();
-        System.out.println("Data Writed");
+        if(f.createNewFile()){
+            System.out.println("File created");
+        }
+        else{
+            System.out.println("File not created");
+        }
         }
         catch(Exception e){
             System.out.println("Exception caught");
         }
+
+        try{
+            FileWriter a = new FileWriter("summa1.txt");
+            a.write("Onu illa!");
+            a.close();
+            System.out.println("Data printed into the file");
+        }
+        catch(IOException c){
+            System.out.println("Exception caught");
+        }
+
+    try{
+    Scanner sc = new Scanner(f);
+    String d = sc.nextLine();
+    System.out.println(d);
+    sc.close();
+    }
+    catch(FileNotFoundException e){
+        System.out.println("FIle not printed");
+    }
+
     }
 }
