8eb8790389b7e84ba5f762d9b60181eaa8aae46c
myleshwarsenthil <myleshwarsenthil@gmail.com>
2026-05-11 19:55:19 +0530

    Overloading a method code

---
+++ overloadEX.java
@@ -0,0 +1,21 @@
+class overloadEX{
+    public static void main(String[] args){
+
+        int a = sum(1,2);
+        int b= sum(1,2,3,4);
+        int c = sum(1,2,3);
+        System.out.println(a+" "+c+" "+b);
+    }
+
+    public static int sum(int a, int b){
+        return(a+b);
+    }
+
+    public static int sum(int a, int b, int c){
+        return(a+b+c);
+    }
+
+    public static int sum(int a, int b, int c, int d){
+        return(a+b+c+d);
+    }
+}
