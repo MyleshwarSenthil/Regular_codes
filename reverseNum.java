b95749ea28fb4e8e3c7e3292296fe7b4af95f656
myleshwarsenthil <myleshwarsenthil@gmail.com>
2026-05-14 20:40:45 +0530

    Reversed number in an array

---
+++ reverseNum.java
@@ -0,0 +1,10 @@
+class reverseNum{
+    public static void main(String[] ag){
+        int [] arr={1,2,3,4,5};
+
+        for(int i=arr.length-1;i>=0;i--){
+            int reversed = arr[i];
+        System.out.println(reversed);
+        }
+    }
+}
